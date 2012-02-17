ee701650959eee291d5e744aad7780420ffca6b6
Oscar <racsonp@yahoo.com>
2012-02-16 23:17:09 -0600

    Add AJAX Calls using jQuery and ASP.Net Demos power by @packtpub

---
+++ Tortuga.Web/AjaxDemo.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Services;
+
+
+namespace Tortuga.Web
+{
+    public partial class AjaxDemo : System.Web.UI.Page
+    {
+        public static string[] UserNameArray;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UserNameArray = new string[3] { "Oscar", "Andrea", "mercedez" };
+        }
+        [WebMethod()]
+        public static bool CheckUserName(string sUserName)
+        {
+            foreach (var strUsername in UserNameArray) 
+            { 
+                if (sUserName == strUsername)         
+                return true;
+            } 
+            return false;
+        }
+    }
+}
